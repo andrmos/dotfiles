d572a2eca97f4432052ac5006bc988f1a9fde1f3
André Mossige <mossige.a@gmail.com>
2019-11-21 15:14:54 +0100

    Legg til setup skript

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# brew install ...
+brew install git neovim fzf tmux htop fish kubectl maven
+
+# brew cask install ...
+brew cask install iterm2 google-chrome slack notion spotify intellij-idea docker sensiblesidebuttons rectangle lastpass firefox karabiner-elements
+
+# node java keyboard language
+
+# dotfiles: vim, ideavimrc, tmuxconf, intellij file watchers, fish history, input mono font
+
+# keyboard speed:
+# defaults write -g InitialKeyRepeat -int 15
+# defaults write -g KeyRepeat -int 2
+
+# intellij config
