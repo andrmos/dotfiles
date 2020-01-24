e49aa5cf5da0b73b4fc2c0fd340634c1f8de556e
André Mossige <mossige.a@gmail.com>
2020-01-24 10:22:35 +0100

    Legg til java, diff, abstract

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-brew install git neovim fzf tmux htop fish kubectl maven node yarn
+brew install git neovim fzf tmux htop fish kubectl maven node yarn diff-so-fancy hub
 
-brew cask install firefox iterm2 slack spotify intellij-idea lastpass notion sensiblesidebuttons rectangle karabiner-elements google-chrome docker
+brew cask install firefox iterm2 slack spotify intellij-idea lastpass notion sensiblesidebuttons rectangle karabiner-elements google-chrome docker java abstract
 
 # dotfiles: vim, ideavimrc, tmuxconf, intellij file watchers, fish history, input mono font
 # keyboard shortcuts bytte vindu
@@ -8,3 +8,6 @@ brew cask install firefox iterm2 slack spotify intellij-idea lastpass notion sen
 # Increase keyboard repeat speed
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
+
+# better diff
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
