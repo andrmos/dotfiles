0d607be31c7e7f4247cc77c06c84660f76fe89f1
André Mossige <andre.mossige@nav.no>
2021-12-14 10:10:36 +0100

    Oppdater programmer

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-brew install git neovim fzf tmux htop fish kubectl maven node yarn diff-so-fancy ripgrep github/gh/gh httpie
+brew install git neovim fzf tmux htop fish kubectl maven node yarn diff-so-fancy ripgrep gh httpie
 
-brew cask install firefox iterm2 slack spotify intellij-idea lastpass sensiblesidebuttons rectangle karabiner-elements google-chrome docker java figma jetbrains-toolbox postman
+brew cask install firefox iterm2 slack spotify intellij-idea 1password sensiblesidebuttons rectangle karabiner-elements google-chrome docker java jetbrains-toolbox postman
 
 # dotfiles: vim, ideavimrc, tmuxconf, intellij file watchers, fish history, input mono font
-# keyboard shortcuts bytte vindu
+# keyboard shortcuts bytte vindu med contexts
 
 # Increase keyboard repeat speed
 defaults write -g InitialKeyRepeat -int 15
